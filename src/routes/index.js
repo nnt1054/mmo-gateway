ec1757af91dbfb511268d0dcd61d4c8dbbd5fc04
Neil Toledo <ntoledo@berkeley.edu>
2021-03-30 02:59:36 -0700

    updated to serve static client-build

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,21 @@
-import { Router } from 'express';
+import path from 'path'
+import express from 'express';
+
 import oktaRoutes from './okta';
 import publicRoutes from './public';
 import blogRoutes from './blog';
 
 export default () => {
-	const app = Router();
+	const app = express.Router();
 
 	oktaRoutes(app);
 	publicRoutes(app);
 	blogRoutes(app);
+
+	app.use('/play', express.static(path.join(__dirname, '../client-build')));
+	app.get('/play', (req, res) => {
+	  res.sendFile(path.join(__dirname, '../client-build', 'index.html'))
+	})
 
 	return app
 }
